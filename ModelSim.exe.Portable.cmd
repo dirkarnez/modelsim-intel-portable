acb84d2e6f62c6e9569c4bfab64e1f2e7c9abac3
Dirk Arnez <smalldirkalex@gmail.com>
2023-09-20 10:20:16 +0800

    - upload files

---
+++ ModelSim.exe.Portable.cmd
@@ -0,0 +1,32 @@
+@echo off
+
+echo %PATH%
+
+set USERPROFILE=%~dp0USERPROFILE
+set DESKTOP=%USERPROFILE%\Desktop
+set APPDATA=%USERPROFILE%\AppData\Roaming
+set LOCALAPPDATA=%USERPROFILE%\AppData\Local
+
+echo %USERPROFILE%
+echo %DESKTOP%
+echo %APPDATA%
+echo %LOCALAPPDATA%
+
+if not exist %USERPROFILE% (
+	mkdir %USERPROFILE%
+)
+
+if not exist %DESKTOP% (
+	mkdir %DESKTOP%
+)
+
+if not exist %APPDATA% (
+	mkdir %APPDATA%
+)
+
+if not exist %LOCALAPPDATA% (
+	mkdir %LOCALAPPDATA%
+)
+
+cd .\18.1\modelsim_ase\win32aloem
+start modelsim.exe
