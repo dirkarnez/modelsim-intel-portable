9470fc01ff9d5b1bc8a260da70a619c55d563b76
Dirk Arnez <smalldirkalex@gmail.com>
2023-09-20 10:12:07 +0800

    - upload files

---
+++ unattended-installation.cmd
@@ -0,0 +1,5 @@
+@echo off
+REM https://stackoverflow.com/questions/37878185/what-does-compat-layer-actually-do
+cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && ModelSimSetup-18.1.0.625-windows.exe --mode unattended --unattendedmodeui none  --installdir "%cd%" --accept_eula 1"
+pause
+
